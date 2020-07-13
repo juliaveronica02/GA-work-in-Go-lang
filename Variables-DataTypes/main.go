d9f5b7502440ed5152f603887121ced971975749
juliaveronica02 <juliavero2206@gmail.com>
2020-07-14 00:06:43 +0700

    Data Types

--- Variables-DataTypes/main.go
+++ Variables-DataTypes/main.go
@@ -10,10 +10,27 @@ func main()  {
 	// int8, int16, int 32 and int64.
 	// float32 and float 64.
 
-	// Using var.
+	// 1. example using var (string).
 	// the green text it's not error, this is data type (string).
 	var myName string = "Julia Veronica"
 	fmt.Println("name: ", myName)
+
+	// 2. example using var (string).
+	var name = "Juve"
+	fmt.Println("short name: ", name)
+	// result: short name:  Juve.
+
+	// 3. example using var (string, integer (int)).
+	var firstName = "Julia"
+	var age int = 19
+	fmt.Println("First Name and age: ", firstName, age)
+	// result: First Name and age:  Julia 19.
+
+	// 4. example using var (string, integer (int)).
+	var firstNames = "Julia"
+	var ages = 19
+	fmt.Println("First Name and age: ", firstNames, ages)
+	// result: First Name and age:  Julia 19.
 }
 // run: go run main.go.
 // result: name:  Julia Veronica.
