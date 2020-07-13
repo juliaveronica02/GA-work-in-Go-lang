d040e550600e67f61c4fae38dafe5fb086d3942e
juliaveronica02 <juliavero2206@gmail.com>
2020-07-13 23:31:26 +0700

    done

--- HelloWorld/main.go
+++ HelloWorld/main.go
@@ -1,8 +1,12 @@
 // file name.
 package main
+// it stands for formatter or format.
+// fmt be able to print stuff on the screen.
 import "fmt"
 
 func main ()  {
+	// print line => Println.
+	// Println same with console.log to show the result on terminal or google console.
 	fmt.Println("Hello World")
 }
 // control flow:
