9cfa00343cf308dff6d90c21535b276fea2ec3c7
juliaveronica02 <juliavero2206@gmail.com>
2020-07-13 23:40:46 +0700

    vars

---
+++ Variables-DataTypes/main.go
@@ -0,0 +1,18 @@
+package main
+
+import "fmt"
+
+func main()  {
+	// Main types:
+	// String.
+	// Bool.
+	// Int.
+	// int8, int16, int 32 and int64.
+	// float32 and float 64.
+
+	// Using var.
+	var myName string = "Julia Veronica"
+	fmt.Println("name: ", myName)
+}
+// run: go run main.go.
+// result: name:  Julia Veronica.
