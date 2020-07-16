6e9c0421ae54fac32cbd4560d564bbce256918cf
juliaveronica02 <juliavero2206@gmail.com>
2020-07-16 10:43:38 +0700

    learn maps

--- Maps/main.go
+++ Maps/main.go
@@ -6,14 +6,14 @@ func main()  {
 	// created with varName := make(map[keyType] valueType).
 	presAge := make(map[string] int)
 	presAge["TheodoreRoosevelt"] = 42
-	fmt.Println(presAge["TheodoreRoosevelt"])
+	fmt.Println("age 42:",presAge["TheodoreRoosevelt"])
 	// get the number of items in the map.
-	fmt.Println(len(presAge))
+	fmt.Println("presAge:",len(presAge))
 	// the size changes when a new item is added.
 	presAge["John F. Kennedy"] = 43
-	fmt.Println(len(presAge))
+	fmt.Println("age 43:",len(presAge))
 	// we can delete by passing the key to delete.
 	delete(presAge, "John F. Kennedy")
-	fmt.Println(len(presAge))
+	fmt.Println("delete:",len(presAge))
 }
 // run: go run main.go.
